8710d0ae113e14b7774739415e5c4092f264079f
AlyxMoon <allisterkmoon@gmail.com>
2021-07-06 16:47:23 -0700

    set up UI with board to display counts/mines

--- src/main.js
+++ src/main.js
@@ -66,3 +66,16 @@ class Cell {
 
 const game = new GameBoard(5, 5, 10);
 console.log(game);
+
+const elBoard = document.querySelector('#game-board');
+elBoard.style.gridTemplateColumns = `repeat(${game.colNum}, var(--cell-size))`;
+
+for (let r = 1; r <= game.rowNum; r++) {
+  for (let c = 1; c <= game.colNum; c++) {
+    const cell = game.board[r][c];
+    // ternary operator : condition ? valueIfTrue : valueIfFalse
+    const cellContent = cell.isBomb ? '.' : cell.adjacentBombs;
+
+    elBoard.innerHTML += `<div class="cell">${cellContent}</div>`;
+  }
+}
